f300ce37b3af328cd42cd44488b2fcf67b0803c2
abdlalisalmi <abdlalisalmi@gmail.com>
2025-01-30 21:59:37 +0100

    Added -- add package configuration

--- apps.py
+++ apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class DjangoAiAssistantConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_ai_assistant'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_ai_assistant"
+    verbose_name = "Django AI Assistant"
+
+    def ready(self):
+        # Add signal handlers or other initialization code
+        pass
